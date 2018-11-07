fad5a8b302deb9b3cc49ad1b0dd71d2f1cf70091
Javapoet <dermot@ispaces.com>
2018-11-07 11:14:00 -0600

    HoloWorld dev and testing

--- HoloWorld/dna/readerWriter/readerWriter.js
+++ HoloWorld/dna/readerWriter/readerWriter.js
@@ -1,26 +1,28 @@
+'use strict';
+
 function genesis () {
   return true;
 }
 
 function validateCommit() {
-  console.log("validateCommit()")
+  //console.log("validateCommit()")
   return true
 }
 
 function validatePut() {
-  console.log("validatePut()")
+  //console.log("validatePut()")
   return true
 }
 
 function holoTextWrite(text) {
-	console.log("holoTextWrite("+text+")")
+	//console.log("holoTextWrite("+text+")")
   var hash = commit('holoText', text);
-	console.log("hash = "+hash)
+	//console.log("hash = "+hash)
   return hash;
 }
 
 function holoTextRead(hash) {
-	console.log("holoTextRead("+hash+")")
+	//console.log("holoTextRead("+hash+")")
   //var holoText = get(hash, { Local: true })
   var holoText = get(hash)
   return holoText
